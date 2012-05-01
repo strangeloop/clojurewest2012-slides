5355e1c5dea006a75f667f412c08706982a916fd
Bill Caputo <bcaputo@drw.com>
2012-05-01 13:02:11 -0500

    added slides for continuous testing in cljure talk

---
+++ Caputo-ContinuousTestingInClojure/03.clj
@@ -0,0 +1,24 @@
+    under the hood...
+    
+    (defproject clojure-west "1.0.0-SNAPSHOT"
+                :description "Sample code and slides for CT talk"
+                :dependencies [[org.clojure/clojure "1.3.0"]]
+                :dev-dependencies [[expectations "1.3.6"]
+                                   [lein-autoexpect "0.0.3"]]
+                :repl-init repl.west)
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    .
+
